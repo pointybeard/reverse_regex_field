5720abf6d5565c8924ff94565f8b262da9eaaec1
Alannah Kearney <hi@alannahkearney.com>
2020-04-09 10:10:51 +1000

    Added checks to make sure composer has been updated and that Extension_Reverse_Regex_Field doesn't already exist

--- extension.driver.php
+++ extension.driver.php
@@ -1,21 +1,35 @@
 <?php
 
-require_once realpath(__DIR__ . "/vendor/autoload.php");
+declare(strict_types=1);
 
-class extension_Reverse_Regex_Field extends Extension
-{
-    public static function init()
-    {
-    }
+if (!file_exists(__DIR__.'/vendor/autoload.php')) {
+    throw new Exception(sprintf(
+        'Could not find composer autoload file %s. Did you run `composer update` in %s?',
+        __DIR__.'/vendor/autoload.php',
+        __DIR__
+    ));
+}
 
-    public function uninstall()
-    {
-        Symphony::Database()->query("DROP TABLE `tbl_fields_reverse_regex`");
-    }
+require_once __DIR__.'/vendor/autoload.php';
 
-    public function install()
+// This file is included automatically in the composer autoloader, however,
+// Symphony might try to include it again which would cause a fatal error.
+// Check if the class already exists before declaring it again.
+if (!class_exists('\\Extension_Reverse_Regex_Field')) {
+    class Extension_Reverse_Regex_Field extends Extension
     {
-        return Symphony::Database()->query("
+        public static function init()
+        {
+        }
+
+        public function uninstall()
+        {
+            Symphony::Database()->query('DROP TABLE `tbl_fields_reverse_regex`');
+        }
+
+        public function install()
+        {
+            return Symphony::Database()->query("
             CREATE TABLE `tbl_fields_reverse_regex` (
                 `id` int(11) unsigned NOT NULL auto_increment,
                 `field_id` int(11) unsigned NOT NULL,
@@ -25,5 +39,6 @@ class extension_Reverse_Regex_Field extends Extension
                 UNIQUE KEY `field_id` (`field_id`)
             )  ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
         ");
+        }
     }
 }
