01a1f67cad942d145fb511a4eea0ccb2903751fd
Alannah Kearney <hi@alannahkearney.com>
2020-04-10 12:39:12 +1000

    Extending AbstractExtension provided by 'Symphony CMS: Extended Base Class Library'

--- extension.driver.php
+++ extension.driver.php
@@ -12,25 +12,25 @@ if (!file_exists(__DIR__.'/vendor/autoload.php')) {
 
 require_once __DIR__.'/vendor/autoload.php';
 
+use pointybeard\Symphony\Extended;
+
 // This file is included automatically in the composer autoloader, however,
 // Symphony might try to include it again which would cause a fatal error.
 // Check if the class already exists before declaring it again.
 if (!class_exists('\\Extension_Reverse_Regex_Field')) {
-    class Extension_Reverse_Regex_Field extends Extension
+    class Extension_Reverse_Regex_Field extends Extended\AbstractExtension
     {
-        public static function init()
-        {
-        }
-
-        public function uninstall()
+        public function uninstall(): bool
         {
             Symphony::Database()->query('DROP TABLE `tbl_fields_reverse_regex`');
         }
 
-        public function install()
+        public function install(): bool
         {
-            return Symphony::Database()->query("
-            CREATE TABLE `tbl_fields_reverse_regex` (
+            parent::install();
+
+            return Symphony::Database()->query(
+                "CREATE TABLE `tbl_fields_reverse_regex` (
                 `id` int(11) unsigned NOT NULL auto_increment,
                 `field_id` int(11) unsigned NOT NULL,
                 `pattern` varchar(100) NOT NULL,
@@ -38,7 +38,8 @@ if (!class_exists('\\Extension_Reverse_Regex_Field')) {
                 PRIMARY KEY  (`id`),
                 UNIQUE KEY `field_id` (`field_id`)
             )  ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
-        ");
+        "
+            );
         }
     }
 }
